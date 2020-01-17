1f3c4323a16a02f02ce7d1b64c9b81c4782ceef5
Debjoy <debjoybhowal7@gmail.com>
2020-01-17 16:18:22 +0530

    added the theme service, module and typescript file

---
+++ src/app/theme/theme.ts
@@ -0,0 +1,49 @@
+export interface Theme{
+    name: string;
+    properties:any;
+}
+
+
+export const dark: Theme={
+    name: "dark",
+    properties: {
+        "--mainbackground-default": "#161c23",
+        "--maintextcolor-default":" #ddd",
+        "--mainsubtitle":"#80bdff",
+    
+        "--inputBoxColor":"#9ecdff",
+    
+        "--primaryColor":"#5294e2",
+        "--primaryDark":"#5294e2",
+    
+        "--SmallBoxShadow":"-2px -2px 3px #2b3744 ,2px 2px 3px #090d10" ,
+        "--LargeBoxShadow":"-4px -4px 5px #2b3744 ,4px 4px 5px #090d10 ",
+        "--MediumBoxShadow":"-3px -3px 4px #2b3744 ,3px 3px 4px #090d10" ,
+    
+        "--inputBoxInsetShadow":"inset 2px 2px 3px #090d10, inset  -2px -2px 3px #2b3744",
+        "--lightThemeBoxShadow":"none",
+        "--darkThemeBoxShadow":"inset 2px 2px 3px #090d10, inset  -2px -2px 3px #2b3744"  
+    }
+}
+
+export const light: Theme={
+    name: "light",
+    properties: {
+        "--mainbackground-default": "#ddd",
+        "--maintextcolor-default":" #161c23",
+        "--mainsubtitle":"#007aff",
+    
+        "--inputBoxColor":"#007cff",
+    
+        "--primaryColor":"#5294e2",
+        "--primaryDark":"#0075ff",
+    
+        "--SmallBoxShadow":"-2px -2px 3px #ffffff ,2px 2px 3px #bfbfbf" ,
+        "--LargeBoxShadow":"-4px -4px 5px #ffffff ,4px 4px 5px #bfbfbf ",
+        "--MediumBoxShadow":"-3px -3px 4px #ffffff ,3px 3px 4px #bfbfbf" ,
+    
+        "--inputBoxInsetShadow":"inset 2px 2px 3px #bfbfbf, inset  -2px -2px 3px #ffffff",
+        "--lightThemeBoxShadow":"none",
+        "--darkThemeBoxShadow":"inset 2px 2px 3px #bfbfbf, inset  -2px -2px 3px #ffffff"
+    }
+}
